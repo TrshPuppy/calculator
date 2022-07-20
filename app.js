15febdd6fdf47ae958df59bcec266df0017438f1
Rose Pineau <rosepineau@protonmail.com>
2022-07-20 12:53:46 -0400

    Add for loop to populate numpad buttons w/ values & data.

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 let numpad = document.querySelector('.numpad')
 numpad.style.gridTemplateColumns = 'repeat(4, 1fr)';
 numpad.style.gridTemplateRows = 'repeat(4, 1fr)';
+numpad.style.gap = '10px';
 
-for(let i = 0; i < 16; i++)
+for(let i = 1; i <=16; i++)
 {
-    let numpadSymbol = 0;
-    let numpadButton = document.createElement('div');
+    let numpadButton = document.createElement('button');
     numpadButton.backgroundColor = 'purple';
+    numpadButton.innerText = i;
+    numpadButton.style.textAlign = 'center';
+    numpadButton.dataset.symbol = i;
     numpad.insertAdjacentElement('beforeend', numpadButton);
 }
