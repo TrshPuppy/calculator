b62a9b7b40f3c226687688b288d5b651f848a089
Rose Pineau <rosepineau@protonmail.com>
2022-07-20 18:00:06 -0400

    Add event listeners to right & left rows and ensure functionality.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Some globals:
+let numberClicked = 0;
+let operatorClicked;
+
 // Generate numpad:
 let numpad = document.querySelector('.numpad')
 numpad.style.gridTemplateColumns = 'repeat(3, 1fr)';
@@ -14,7 +18,6 @@ for(let i = 1; i <=9; i++)
     numpad.insertAdjacentElement('beforeend', numpadButton);
 
     // Add event listeners to numpad buttons:
-    let numberClicked = 0;
     numpadButton.addEventListener('click', e =>
     {
         numberClicked = parseInt(e.target.dataset.symbol);
@@ -24,4 +27,32 @@ for(let i = 1; i <=9; i++)
 
 // Add event listeners to right row operators:
 let rightRowButtons = document.querySelectorAll('.right-row button');
+rightRowButtons.forEach(button =>
+    {
+        button.addEventListener('click', e =>
+        {
+            operatorClicked = e.target.dataset.symbol;
+            console.log(operatorClicked);
+        })
+    })
 console.log(rightRowButtons);
+
+// Add event listeners to bottom row:
+let bottomRowButtons = document.querySelectorAll('.bottom-row button')
+bottomRowButtons.forEach(button =>
+    {
+        button.addEventListener('click', e =>
+        {
+            if(e.target.dataset.symbol == "0" || e.target.dataset.symbol == ".")
+            {
+                numberClicked = e.target.dataset.symbol;
+                console.log(numberClicked);
+            }
+            else
+            {
+                operatorClicked = e.target.dataset.symbol;
+                console.log(operatorClicked);
+            }
+        });
+    });
+console.log(bottomRowButtons);
