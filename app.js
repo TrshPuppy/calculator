379f798c7854d2b14d39806dc55b035dce60a76d
Rose Pineau <rosepineau@protonmail.com>
2022-07-20 17:33:01 -0400

    Add event listeners to numpad buttons and right row.

--- app.js
+++ app.js
@@ -12,4 +12,16 @@ for(let i = 1; i <=9; i++)
     numpadButton.style.textAlign = 'center';
     numpadButton.dataset.symbol = i;
     numpad.insertAdjacentElement('beforeend', numpadButton);
+
+    // Add event listeners to numpad buttons:
+    let numberClicked = 0;
+    numpadButton.addEventListener('click', e =>
+    {
+        numberClicked = parseInt(e.target.dataset.symbol);
+        console.log(numberClicked);
+    });
 }
+
+// Add event listeners to right row operators:
+let rightRowButtons = document.querySelectorAll('.right-row button');
+console.log(rightRowButtons);
