e3371a373d12d81a1ef85d5b91ad665be7399217
Rose Pineau <rosepineau@protonmail.com>
2022-07-20 17:01:09 -0400

    Add and style remaining buttons.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Generate numpad:
 let numpad = document.querySelector('.numpad')
-numpad.style.gridTemplateColumns = 'repeat(4, 1fr)';
-numpad.style.gridTemplateRows = 'repeat(4, 1fr)';
+numpad.style.gridTemplateColumns = 'repeat(3, 1fr)';
+numpad.style.gridTemplateRows = 'repeat(3, 1fr)';
 numpad.style.gap = '10px';
 
-for(let i = 1; i <=16; i++)
+for(let i = 1; i <=9; i++)
 {
     let numpadButton = document.createElement('button');
     numpadButton.backgroundColor = 'purple';
