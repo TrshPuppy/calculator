27ec0a99752bd069ad8d7c7bbbd1dea11caea462
Rose Pineau <rosepineau@protonmail.com>
2022-07-21 13:27:07 -0400

    Define global variables and calculator class.

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
+// Calculator blueprint:
+class Calculator
+{
+    constructor(previousScreen, currentScreen)
+    {
+        this.previousScreen = previousScreen;
+        this.currentScreen = currentScreen;
+    }
+
+    // Clear screen
+    clearScreen()
+    {
+        this.currentScreen = '';
+        this.previousScreen = '';
+        this.operation = undefined;
+    }
+    // Delete single number from current screen:
+    deleteNumber()
+    {
+
+    }
+    // Append number to current screen:
+    appendNumber(num)
+    {
+
+    }
+    // Choose operator
+    chooseOperator(operator)
+    {
+
+    }
+    // Compute operation
+    compute()
+    {
+
+    }
+    // Update screen
+    updateScreen()
+    {
+
+    }
+}
+
+// Buttons:
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const clearButton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const equalsButton = document.querySelector('[data-equals]');
+
+// Screen:
+const previousScreen = document.querySelector('[data-previous-operand]');
+const currentScreen = document.querySelector('[data-current-operand]');
 
