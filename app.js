7b748d38acbc8387f6e8632bbafa29abfc4c99c6
Rose Pineau <rosepineau@protonmail.com>
2022-07-21 18:27:50 -0400

    Compute function works.

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Calculator blueprint:
 class Calculator
 {
-    constructor(previousScreenText, currentScreenText)
+    constructor(previousScreenText, currentScreenText, currentOperator)
     {
         this.previousScreenText = previousScreenText;
         this.currentScreenText = currentScreenText;
     }
+
+    
 
     // Clear screen
     clearScreen()
@@ -20,24 +22,47 @@ class Calculator
     // Append number to current screen:
     appendNumber(num)
     {
+        if(num === '.' && currentScreenText.textContent.includes('.')) return;
         currentScreenText.textContent += num;
-        //console.log(currentScreenText.textContent);
     }
     // Choose operator
     chooseOperator(operator)
     {
-        previousScreenText.textContent += (currentScreenText.textContent + operator);
-        this.updateScreen();
-        console.log(operator);
+        currentOperator = operator;
+        console.log(currentOperator);
     }
     // Compute operation
     compute()
     {
-
+        console.log('click');
+        let computation;
+        let previous = parseFloat(this.previousScreenText.textContent);
+        console.log(previous);
+        let current = parseFloat(this.currentScreenText.textContent);
+        console.log(current);
+        switch (currentOperator)
+        {
+            case '+':
+                computation = previous + current
+                break
+            case '-':
+                computation = previous - current
+                break
+            case '*':
+                computation = previous * current
+                break
+            case '÷':
+                computation = previous / current
+                break
+            default:
+                return
+        }
+        console.log(computation);
     }
     // Update screen
     updateScreen()
     {
+        previousScreenText.textContent += currentScreenText.textContent + currentOperator;
         currentScreenText.textContent = '';
     }
 }
@@ -48,13 +73,15 @@ const operatorButtons = document.querySelectorAll('[data-operator]');
 const clearButton = document.querySelector('[data-clear]');
 const deleteButton = document.querySelector('[data-delete]');
 const equalsButton = document.querySelector('[data-equals]');
+let currentOperator = undefined;
+let operationString = '';
 
 // Screen:
 const previousScreenText = document.querySelector('[data-previous-operand]');
 const currentScreenText = document.querySelector('[data-current-operand]');
 
 // Create calculator instance:
-const calculator = new Calculator(previousScreenText, currentScreenText);
+const calculator = new Calculator(previousScreenText, currentScreenText, currentOperator);
 
 // Add event listeners to buttons:
 numberButtons.forEach(number =>
@@ -70,11 +97,12 @@ operatorButtons.forEach(button =>
         button.addEventListener('click', () =>
         {
             calculator.chooseOperator(button.textContent);
+            calculator.updateScreen();
         })
     })
 
-// equalsButton.addEventListener('click', () =>
-// {
-//     calculator.compute();
-// })
+equalsButton.addEventListener('click', () =>
+{
+    calculator.compute();
+})
 
